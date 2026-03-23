626e12287f1c19b813e5c13cc38bd91809fa4de4
vaibhavsrv <srvvaibhav8@gmail.com>
2026-03-23 22:14:21 +0530

    Starting of the Project

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const authRoutes = require('./routes/auth')
+
+const app = express()
+app.use(cors)
+app.use(express.json)
+
+app.use('/api/auth' , authRoutes)
+
+app.get('/' , (req,res) => res.send("Cura API is running"))
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,() => console.log("Server is running on PORT ${PORT}"))
+
+```
+** `.env`** 
+```
+
+PORT = 5000
+JWT_SECRET = cura_super_secret_key_change_this
+
